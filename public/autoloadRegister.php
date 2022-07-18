f0861aa8920a67786dc45640ecb2132f1dbbf56f
Alexsey Molozhenko <jukov20150@gmail.com>
2022-07-18 18:33:22 +0300

    autoloadregister

---
+++ public/autoloadRegister.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once '../config/configAutoloadRegister.php';
+
+
+/**
+ * implementation of class autoloading function
+ */
+spl_autoload_register(function($class){
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    $classPath = DEFAULT_DIRECTORY_NAME.DIRECTORY_SEPARATOR.$class.'.php';
+    if(file_exists($classPath)){
+        include_once $classPath;
+        return true;
+    }
+    return false;
+
+});
+
+
+Route::init();
