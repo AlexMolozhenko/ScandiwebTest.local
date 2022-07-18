15949803384c618cdef2e2e5ac34da445b8f869d
Alexsey Molozhenko <jukov20150@gmail.com>
2022-07-18 18:50:30 +0300

    config

---
+++ config/configAutoloadRegister.php
@@ -0,0 +1,5 @@
+<?php
+
+const DEFAULT_DIRECTORY_NAME = '../App' ;
+
+
---
+++ config/configRoute.php
@@ -0,0 +1,15 @@
+<?php
+
+const DEFAULT_CONTROLLER_NAME= 'ProductList' ;
+
+const DEFAULT_ACTION_NAME = 'index';
+
+/**
+ * converts the first letter to capital
+ * @param $str
+ * @param string $encoding
+ * @return string
+ */
+function mb_ucfirst($str, $encoding = 'UTF-8') {
+    return mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding) . mb_strtolower(mb_substr($str, 1, mb_strlen($str), $encoding), $encoding);
+}
