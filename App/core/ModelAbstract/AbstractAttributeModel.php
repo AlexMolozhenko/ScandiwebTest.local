65a9853708c227e8708cf11102fe23a321400853
Alexsey Molozhenko <jukov20150@gmail.com>
2022-07-21 01:11:03 +0300

    abstract(AttributeModel,ProductsModel,ProductTypeAttributeModel,TypeAttributeModel,TypeModel)

---
+++ App/core/ModelAbstract/AbstractAttributeModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace core\ModelAbstract;
+
+use core\ModelAbstract\AbstractModel;
+
+abstract class AbstractAttributeModel extends AbstractModel
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function add($data)
+    {
+        // TODO: Implement add() method.
+    }
+}
---
+++ App/core/ModelAbstract/AbstractProductsModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace core\ModelAbstract;
+
+
+
+abstract class AbstractProductsModel extends AbstractModel
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function add($data)
+    {
+        // TODO: Implement add() method.
+    }
+}
