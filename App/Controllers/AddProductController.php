b7bce621c6bdf5acb5820bda63b1096927b51274
Alexsey Molozhenko <jukov20150@gmail.com>
2022-07-24 20:42:42 +0300

    addproductcontroller

--- App/Controllers/AddProductController.php
+++ App/Controllers/AddProductController.php
@@ -16,30 +16,30 @@ class AddProductController extends AbstractController
 
     public function index(){
 
-        $typeAll = $this->typeModel->getAll();
-        $typeId = $this->typeModel->get(1);
-        $attributeAll = $this->attributeModel->getAll();
-        $attributeId = $this->attributeModel->get(1);
-        $IdTypeAttributeByTypeId = $this->typeAttributeModel->getIdTypeAttributeByTypeId(3);
-        $productAdd = $this->productsModel->add('test','testNAME',50,2);
-        $productsAll =$this->productsModel->getAll();
-        $productId =$this->productsModel->get(3);
-//        $productTypeAttribute = $this->productTypeAttributeModel->
-
+//        $typeAll = $this->typeModel->getAll();
+//        $typeId = $this->typeModel->get(1);
+//        $attributeAll = $this->attributeModel->getAll();
+//        $attributeId = $this->attributeModel->get(1);
+//        $IdTypeAttributeByTypeId = $this->typeAttributeModel->get(3);
+//        $productAdd = $this->productsModel->add('t3','testNAME33',32,3);
+//        $productsAll =$this->productsModel->getAll();
+//        $productId =$this->productsModel->get(3);
+        $productTypeAttribute = $this->productTypeAttributeModel->add(17,15,300);
 
 
 
 
 
         $this->view->render('addProduct_index_view',[
-            'typeAll'=>$typeAll,
-            'typeId'=>$typeId,
-            'attributeAll'=>$attributeAll,
-            'attributeId'=>$attributeId,
-            'IdTypeAttributeByTypeId'=>$IdTypeAttributeByTypeId,
-            'productAdd'=>$productAdd,
-            'productsAll'=>$productsAll,
-            'productId'=>$productId,
+//            'typeAll'=>$typeAll,
+//            'typeId'=>$typeId,
+//            'attributeAll'=>$attributeAll,
+//            'attributeId'=>$attributeId,
+//            'IdTypeAttributeByTypeId'=>$IdTypeAttributeByTypeId,
+//            'productAdd'=>$productAdd,
+//            'productsAll'=>$productsAll,
+//            'productId'=>$productId,
+           'productTypeAttribute'=>$productTypeAttribute,
         ]);
 
 
--- App/View/user/addProduct_index_view.php
+++ App/View/user/addProduct_index_view.php
@@ -1,10 +1,11 @@
 <?php
 echo' add product ';
-var_dump($typeAll);
-var_dump($typeId);
-var_dump($attributeAll);
-var_dump($attributeId);
-var_dump($IdTypeAttributeByTypeId);
-var_dump($productAdd);
-var_dump($productsAll);
-var_dump($productId);
+//var_dump($typeAll);
+//var_dump($typeId);
+//var_dump($attributeAll);
+//var_dump($attributeId);
+//var_dump($IdTypeAttributeByTypeId);
+//var_dump($productAdd);
+//var_dump($productsAll);
+//var_dump($productId);
+var_dump($productTypeAttribute);
