d6bfe055546bc855df442185d920c486f684364a
Alexsey Molozhenko <jukov20150@gmail.com>
2022-07-31 22:00:17 +0300

    js

--- public/JS/addProduct.js
+++ public/JS/addProduct.js
@@ -18,22 +18,39 @@
      e.preventDefault();
  };
 
- document.getElementById('save').onclick = function(e){
-     let form = document.forms.product_form;
-     let formData = new FormData(form);
-     let url = "AddProduct/add";
-     let xhr = new XMLHttpRequest();
-     xhr.open('POST',url);
-     xhr.send(formData);
-     xhr.onreadystatechange = function(){
-         if(xhr.readyState === 4){
-             if(xhr.status === 200){
-                 alert('ok');
-                 document.getElementById('response').innerHTML = this.responseText;
-             }
-         }
-     };
-     // console.log(form);
-     e.preventDefault();
- };
+ // document.getElementById('save').onclick = function(e){
+ //     let form = document.forms.product_form;
+ //     let formData = new FormData(form);
+ //
+ //     let errorForm = [];
+ //     for(let el of formData.entries()){
+ //
+ //         if(el[1].length === 0){
+ //             errorForm[el[0]] = "data in the line "+el[0]+" is not entered or entered incorrectly";
+ //         }
+ //     }
+ //     console.log(errorForm);
+ //     // for(let el of form.elements){
+ //     //     let errorForm = [];
+ //     //     if(el.value == 0){
+ //     //         errorForm[el.name] = "data in the line "+el.name+" is not entered or entered incorrectly";
+ //     //         console.log(errorForm[el.name]);
+ //     //     }
+ //     //       console.log(el.value);
+ //     //   }
+ //     // let formData = new FormData(form);
+ //     // let xhr = new XMLHttpRequest();
+ //     // let method = form.method;
+ //     // let url = form.action;
+ //     // xhr.open(method,url);
+ //     // xhr.send(formData);
+ //     // xhr.onreadystatechange = function(){
+ //     //     if(xhr.readyState === 4){
+ //     //         if(xhr.status === 200){
+ //     //             document.getElementById('response').innerHTML = this.responseText;
+ //     //         }
+ //     //     }
+ //     // };
+ //     e.preventDefault();
+ // };
 
