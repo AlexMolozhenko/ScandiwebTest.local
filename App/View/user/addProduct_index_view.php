ed2df92104493b35f8558c45bc547c600259e410
Alexsey Molozhenko <jukov20150@gmail.com>
2022-08-01 23:11:43 +0300

    view

--- App/View/user/addProduct_index_view.php
+++ App/View/user/addProduct_index_view.php
@@ -1,25 +1,25 @@
 
 <nav>
     <h2>Product Add</h2>
-    <button type="submit" id="save" form="product_form">Save</button>
+    <button type="button" id="save" form="product_form">Save</button>
     <button>Cancel</button>
 </nav>
 
 <form action="<?= Route::url('AddProduct','add')?>" method="POST" enctype="application/x-www-form-urlencoded" name="product_form" id="product_form" >
     <label>SKU
-        <input type="text" name="sku"  placeholder="Please, provide SKU" required pattern="^[a-zA-Z0-9]+$" minlength="5" id="productSku" />
+        <input type="text" name="sku"    pattern="^[a-zA-Z0-9]+$"  required id="productSku" />
     </label>
     <label>Name
-        <input type="text" name="name"  placeholder="Please, provide Name" required pattern="^[a-zA-Z0-9]+$" id="productName" minlength="3"/>
+        <input type="text" name="name"   required pattern="^[a-zA-Z0-9]+$" id="productName" />
     </label>
     <label>Price ($)
-        <input type="number" name="price"  placeholder="Please, provide Price" required id="productPrice"/>
+        <input type="number" step="any" name="price" pattern="\-?\d+[.,]\d"  required id="productPrice"/>
     </label>
     <label>Type Switcher
         <select id="productType" required>
-                <option selected disabled>Type Switcher</option>
+                <option value="">Type Switcher</option>
             <?php foreach ($types as $type):?>
-                <option  name="<?=$type['name']?>" value="<?=$type['id']?>"><?=$type['name']?></option>
+                <option  name="<?=$type['name']?>" value="<?=$type['id']?>" ><?=$type['name']?></option>
             <?php endforeach ;?>
         </select>
     </label>
--- App/View/user/includes/attribute.php
+++ App/View/user/includes/attribute.php
@@ -5,6 +5,6 @@
 
     <?php foreach ($attributes as $attribute):?>
        <label><?=$attribute['name']?> (<?=$attribute['units']?>)
-           <input type="number" name="attributes[<?=$attribute['id']?>]" id="<?=$attribute['id']?>" form="product_form"  placeholder="Please, provide <?=$attribute['name']?>" required/>
+           <input type="number" step="any" name="attributes[<?=$attribute['id']?>]" id="<?=$attribute['id']?>" form="product_form"  pattern="\-?\d+[.,]\d" required/>
        </label>
     <?php endforeach;?>
