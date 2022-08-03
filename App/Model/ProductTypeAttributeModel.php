a28c13c454d300745d461746b362c5723c850875
Alexsey Molozhenko <jukov20150@gmail.com>
2022-08-03 21:44:47 +0300

    model

--- App/Model/ProductTypeAttributeModel.php
+++ App/Model/ProductTypeAttributeModel.php
@@ -10,12 +10,22 @@ class ProductTypeAttributeModel extends AbstractModel implements InterfaceProduc
 {
     public function getAll()
     {
-        // TODO: Implement getAll() method.
+        $sql = "SELECT `products_type_attribute`.`products_id` as `productId`,`attribute`.`name` as `attributeName`,`attribute`.`units` as `atributeUnits`,`products_type_attribute`.`value` as `attributeValue` FROM `products_type_attribute` INNER JOIN `type_attribute` ON `products_type_attribute`.`type_attribute_id` = `type_attribute`.`id` INNER JOIN `attribute` ON `type_attribute`.`attribute_id` = `attribute`.`id`;";
+        $result = $this->db->query($sql);
+        if($this->db->errno !== 0  ){
+            throw new \Exception($this->db->error);
+        }
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     public function get($id)
     {
-        // TODO: Implement get() method.
+//        $sql = "SELECT `attribute`.`name` as `attributeName`,`attribute`.`units` as `atributeUnits`,`products_type_attribute`.`value` as `attributeValue` FROM `products_type_attribute` INNER JOIN `type_attribute` ON `products_type_attribute`.`products_id`=`$id` AND `products_type_attribute`.`type_attribute_id` = `type_attribute`.`id` INNER JOIN `attribute` ON `type_attribute`.`attribute_id`=`attribute`.`id`;";
+//        $result = $this->db->query($sql);
+//        if($this->db->errno !== 0  ){
+//            throw new \Exception($this->db->error);
+//        }
+//        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     /**
--- App/Model/ProductsModel.php
+++ App/Model/ProductsModel.php
@@ -4,6 +4,7 @@ namespace Model;
 
 use core\ImplementsModel\InterfaceProductsModel;
 use core\ModelAbstract\AbstractModel;
+use Exception\MySQLException;
 
 class ProductsModel extends AbstractModel implements InterfaceProductsModel
 {
@@ -15,6 +16,8 @@ class ProductsModel extends AbstractModel implements InterfaceProductsModel
     public function getAll()
     {
         $sql = "SELECT * FROM `products`;";
+//        $sql = "SELECT `products`.`id` as `productId`,`products`.`sku` as `productSku`,`products`.`name` as `productName`,`products`.`price`,`products`.`currency`,`attribute`.`name` as `attributeName`,`attribute`.`units` as `atributeUnits`,`products_type_attribute`.`value` as `attributeValue` FROM `products` INNER JOIN  `products_type_attribute` ON `products_type_attribute`.`products_id`=`products`.`id`INNER JOIN `type_attribute` ON `type_attribute`.`id`=`products_type_attribute`.`type_attribute_id` INNER JOIN `attribute` ON `type_attribute`.`attribute_id`=`attribute`
+//.`id`;";
         $result = $this->db->query($sql);
         if($this->db->errno !== 0  ){
             throw new \Exception($this->db->error);
@@ -30,12 +33,12 @@ class ProductsModel extends AbstractModel implements InterfaceProductsModel
      */
     public function get($id)
     {
-        $sql = "SELECT * FROM `products` WHERE `id`={$id};";
-        $result = $this->db->query($sql);
-        if($this->db->errno !== 0  ){
-            throw new \Exception($this->db->error);
-        }
-        return $result->fetch_all(MYSQLI_ASSOC);
+//        $sql = "SELECT * FROM `products` WHERE `id`={$id};";
+//        $result = $this->db->query($sql);
+//        if($this->db->errno !== 0  ){
+//            throw new \Exception($this->db->error);
+//        }
+//        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     /**
@@ -52,7 +55,7 @@ class ProductsModel extends AbstractModel implements InterfaceProductsModel
         $sql = "INSERT INTO `products` (sku,name,price,type_id) VALUES ('{$sku}','{$name}','{$price}','{$type_id}');";
         $this->db->query($sql);
         if($this->db->errno !== 0  ){
-            throw new \Exception($this->db->error);
+            throw new MySQLException($this->db->error);
         }else{
             return $this->db->insert_id;
         }
