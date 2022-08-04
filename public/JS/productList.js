f9349a54aa0beb74f0caa21dfec804cf8bc9365e
Alexsey Molozhenko <jukov20150@gmail.com>
2022-08-04 22:42:57 +0300

    productList.js

--- public/JS/productList.js
+++ public/JS/productList.js
@@ -6,3 +6,27 @@ document.getElementById('add').onclick = function(e){
     redirect('addProduct')
     e.preventDefault();
 }
+
+/**
+ * sends an array of products and reloads the page
+ * @param e
+ */
+document.getElementById('delete-product-btn').onclick = function(e){
+    alert('ok');
+    let form = document.forms.delete_form;
+    let formData = new FormData(form);
+    let xhr = new XMLHttpRequest();
+    let method = form.method;
+    let url = form.action;
+    xhr.open(method,url);
+    xhr.send(formData);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                formReset(form);
+                redirect();
+            }
+        }
+    }
+    e.preventDefault();
+}
