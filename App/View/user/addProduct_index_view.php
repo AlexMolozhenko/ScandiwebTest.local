bb6393a15edc023f3a9497e9d449b1f52cdf27a4
Alexsey Molozhenko <jukov20150@gmail.com>
2022-08-04 22:42:37 +0300

    view

--- App/View/user/addProduct_index_view.php
+++ App/View/user/addProduct_index_view.php
@@ -4,27 +4,29 @@
     <button type="button" id="save" form="product_form">Save</button>
     <button type="button" id="cansel" form="product_form">Cancel</button>
 </nav>
-
-<form action="<?= Route::url('AddProduct','add')?>" method="POST" enctype="application/x-www-form-urlencoded" name="product_form" id="product_form" >
-    <label>SKU
-        <input type="text" name="sku"    pattern="^[a-zA-Z0-9\/]+$"  required id="productSku" />
-    </label>
-    <label>Name
-        <input type="text" name="name"   required pattern="^[a-zA-Z0-9\s]+$" id="productName" />
-    </label>
-    <label>Price ($)
-        <input type="number" step="any" name="price" pattern="\-?\d+[.,]\d"  required id="productPrice"/>
-    </label>
-    <label>Type Switcher
-        <select id="productType" required>
+<div class="">
+    <form action="<?= Route::url('AddProduct','add')?>" method="POST" enctype="application/x-www-form-urlencoded" name="product_form" id="product_form" >
+        <label>SKU
+            <input type="text" name="sku"    pattern="^[a-zA-Z0-9\/]+$"  required id="productSku" />
+        </label>
+        <label>Name
+            <input type="text" name="name"   required pattern="^[a-zA-Z0-9\s]+$" id="productName" />
+        </label>
+        <label>Price ($)
+            <input type="number" step="any" name="price" pattern="\-?\d+[.,]\d"  required id="productPrice"/>
+        </label>
+        <label>Type Switcher
+            <select id="productType" required>
                 <option value="">Type Switcher</option>
-            <?php foreach ($types as $type):?>
-                <option  name="<?=$type['name']?>" value="<?=$type['id']?>" ><?=$type['name']?></option>
-            <?php endforeach ;?>
-        </select>
-    </label>
-    <div id="attribute"></div>
-</form>
+                <?php foreach ($types as $type):?>
+                    <option  name="<?=$type['name']?>" value="<?=$type['id']?>" ><?=$type['name']?></option>
+                <?php endforeach ;?>
+            </select>
+        </label>
+        <div id="attribute"></div>
+    </form>
+</div>
+
 
 <div id="errorMassage">
    <?php if(!empty($errors)):?>
--- App/View/user/productList_index_view.php
+++ App/View/user/productList_index_view.php
@@ -3,11 +3,33 @@
 <nav>
     <h2>Product list</h2>
     <button type="button" id="add" >ADD</button>
-    <button type="button" id="massDelete" >Mass Delete</button>
+    <button type="button" id="delete-product-btn" form="delete-form" >Mass Delete</button>
 </nav>
-<?php foreach($productsAtr as $product):?>
-<?php var_dump($product);?>
-<?php endforeach;?>
+<form action="<?= Route::url('ProductList','destroy')?>" method="POST" enctype="application/x-www-form-urlencoded" id="delete_form">
+    <div >
+        <?php foreach($products as $product):?>
+            <div class="product">
+                <?php foreach($product as $key=>$values):?>
+
+                    <?php if($key == 'product'):?>
+                    <input type="checkbox"  name="products[]" value="<?=$values['id']?>" class="delete-checkbox"/>
+                        <div><?=$values['sku']?></div>
+                        <div><?=$values['name']?></div>
+                        <div><span><?=$values['price'].' '.$values['currency']?></span></div>
+                    <?php endif;?>
+
+                    <?php if($key == 'attributes'):?>
+                        <?php foreach($values as $attribute):?>
+                            <div><span><?=$attribute['attributeName'].' '.$attribute['attributeValue'].''.$attribute['attributeUnits']?></span></div>
+                        <?php endforeach;?>
+                    <?php endif;?>
+                <?php endforeach;?>
+            </div>
+        <?php endforeach;?>
+    </div>
+</form>
+
+
 
 
 
