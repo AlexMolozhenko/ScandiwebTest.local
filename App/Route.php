828c3523a095719de7ec4792620b449ecefd397e
Alexsey Molozhenko <jukov20150@gmail.com>
2022-08-15 18:04:06 +0300

    route

--- App/Route.php
+++ App/Route.php
@@ -17,14 +17,14 @@ class Route
 //======
         function pr($val)
         {
-            $bt = debug_backtrace();
-            $file = file($bt[0]['file']);
-            $src = $file[$bt[0]['line'] - 1];
-            $pat = '#(.*)' . __FUNCTION__ . ' *?\( *?(.*) *?\)(.*)#i';
-            $var = preg_replace($pat, '$2', $src);
-            echo '<script>console.log("' . trim($var) . '=' .
-                addslashes(json_encode($val, JSON_UNESCAPED_UNICODE)) . '")</script>' . "\n";
-    echo '<script>console.log("' .$val.'")</script>' . "\n";
+//            $bt = debug_backtrace();
+//            $file = file($bt[0]['file']);
+//            $src = $file[$bt[0]['line'] - 1];
+//            $pat = '#(.*)' . __FUNCTION__ . ' *?\( *?(.*) *?\)(.*)#i';
+//            $var = preg_replace($pat, '$2', $src);
+//            echo '<script>console.log("' . trim($var) . '=' .
+//                addslashes(json_encode($val, JSON_UNESCAPED_UNICODE)) . '")</script>' . "\n";
+    echo '<script>console.log("' .$val.'")</script>';
         }
         pr($uri);
 ////====
