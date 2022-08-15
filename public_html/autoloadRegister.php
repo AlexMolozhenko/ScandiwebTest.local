83bba5b3c38e7a9cc028e07bc078f55a432a8b32
Alexsey Molozhenko <jukov20150@gmail.com>
2022-08-15 18:36:59 +0300

    autoload var_dump classpass

--- public_html/autoloadRegister.php
+++ public_html/autoloadRegister.php
@@ -8,8 +8,9 @@ include_once '..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'configAutolo
  */
 spl_autoload_register(function($class){
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    var_dump($class);
+
     $classPath = DEFAULT_DIRECTORY_NAME.DIRECTORY_SEPARATOR.$class.'.php';
+    var_dump($classPath);
     if(file_exists($classPath)){
         include_once $classPath;
         return true;
